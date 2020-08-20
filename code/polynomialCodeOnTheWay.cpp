2942d7100072775f2607d1033f2aeadd0b9c7ebc
Nitanshu Lokhande <56120084+nlok5923@users.noreply.github.com>
2020-08-19 20:07:14 -0700

    Update polynomialCodeOnTheWay.cpp

--- code/polynomialCodeOnTheWay.cpp
+++ code/polynomialCodeOnTheWay.cpp
@@ -1,35 +1,32 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 #include<bits/stdc++.h>
 using namespace std;
-class polynomial{
+class Polynomial{
+    public:
     int* degCoeff;
     int capacity;
+  
     public:
     
-    polynomial()
+    Polynomial()
     {
-        degCoeff = new int[5];
-        capacity = 5;
+        degCoeff = new int[100];
+        capacity = 100;
         int i=0;
-        for(i=0;i<5;i++)
+        for(i=0;i<100;i++)
         degCoeff[i] = 0;
+       
     } 
-    polynomial(int length)
+    Polynomial(int length)
     {
         degCoeff = new int[length];
         capacity =length;
         int i=0;
         for(i=0;i<length;i++)
-        degCoeff[i] =0;
+        degCoeff[i] = 0;
+    
     }
-  //  p1 originala and p2 copy karna hai this p2 ka hai 
-    polynomial( polynomial const &p1)
+    Polynomial( Polynomial const &p1)
     {
          int i=0;
          this->degCoeff = new int[p1.capacity];
@@ -40,128 +37,180 @@ class polynomial{
          this->capacity = p1.capacity;
         
     }
-   
-    //print
-    void display()
+    void print()
     {
         int i=0;
-        for(i=0;i<capacity;i++)
+        int j=0;
+        int t=0;
+        for(j=capacity-1;j>=0;j--)
         {
-            if(i!=0)
+            if(degCoeff[j]!=0)
             {
-                //cout<<degCoeff[i]<<"x"<<i;
-                if(degCoeff[i]>0)
-                cout<<"+"<<degCoeff[i]<<"x";
-                else
-                 cout<<"-"<<degCoeff[i]<<"x";
+                t = j+1;
+            break;
             }
+            
+        }
+        for(i=0;i<t;i++)
+        {
+                if(degCoeff[i]!=0)
+                cout<<degCoeff[i]<<"x"<<i<<" ";
+            
             
         }
         cout<<endl;
     }
-    //overloading three operator +,,-,*
-    //+ will all two polynomial;
-    //add p1 and p2 and saove in p3
-    //p1 = p2+p3 p2 ke liye call hua and p3 send kiya
-    polynomial operator+(polynomial const &p3)
+  
+    void operator=(Polynomial const &d){
+
+        this->degCoeff = new int[d.capacity];
+        for(int i=0;i<d.capacity;i++){
+            this->degCoeff[i] = d.degCoeff[i];
+        }
+        this->capacity = d.capacity;
+        
+        
+    }
+    Polynomial operator+(Polynomial p3)
     {
-        int i=0,j=0,temp=0;
-        //int *newArray;
-        polynomial temp(this->capacity);
+        int i=0,j=0;
+       
+            
         if(this->capacity > p3.capacity)
         {
-            //temp.newArray = new int[this->capacity];
+            Polynomial temp(this->capacity);
+            
             for(i=0;i<p3.capacity;i++)
             {
                 temp.degCoeff[i] = this->degCoeff[i] + p3.degCoeff[i];
             }
             for(j=p3.capacity;j<this->capacity;j++)
             temp.degCoeff[j] = this->degCoeff[i];
+            return temp;
             
         }
         else
         {
-           
+            Polynomial temp(p3.capacity);
             for(i=0;i<this->capacity;i++)
            temp.degCoeff[i]= this->degCoeff[i]+p3.degCoeff[i];
             for(j=this->capacity;j<p3.capacity;j++)
             temp.degCoeff[i] = p3.degCoeff[i];
+           
+             return temp;
         }
         
-        return temp;
+       
         
         
     }
-    polynomial operator-(polynomial const &p3)
+    Polynomial operator-(Polynomial const &p3)
     {
-        int i=0,j=0,temp=0;
-      //  int *newArray;
-      polynomial temp(this->capacity);
+        int i=0,j=0,temp=0;  
         if(this->capacity > p3.capacity)
         {
             //newArray = new int[this->capacity];
+               Polynomial temp(this->capacity);
             for(i=0;i<p3.capacity;i++)
             {
                 temp.degCoeff[i] = this->degCoeff[i] - p3.degCoeff[i];
             }
             for(j=p3.capacity;j<this->capacity;j++)
             temp.degCoeff[j] = this->degCoeff[i];
+             return temp;
             
         }
         else
         {
+             Polynomial temp(p3.capacity);
             //newArray = new int[p3.capacity];
             for(i=0;i<this->capacity;i++)
-        temp.degCoeff[i]= this->degCoeff[i] - p3.degCoeff[i];
+           temp.degCoeff[i]= this->degCoeff[i] - p3.degCoeff[i];
             for(j=this->capacity;j<p3.capacity;j++)
-        temp.degCoeff[j] = p3.degCoeff[i];
+            temp.degCoeff[j] = p3.degCoeff[i];
+             return temp;
         }
         
-        return newArray;
+       
         
     }
     
-    polynomial operator*(polynomial const &p3)
+    Polynomial operator*(Polynomial const &p3)
     {
         //p2 this wala
         //p3 explicit wala
         int i=0,j=0;
         //int **newArray = new int[this.capacity];
         
-        polynomial p1[this->capacity](this->capacity +p3.capacity);
-        
+        //Polynomial p1[this->capacity](this->capacity + p3.capacity);
+        Polynomial p1(this->capacity+ p3.capacity);
+        Polynomial empty(this->capacity + p3.capacity);
+
+
         while(i!=this->capacity)
         {
+            Polynomial p2(this->capacity+p3.capacity);
             if(i==0)
             {
             for(j=0;j<p3.capacity;j++)
-            p1.degCoeff[j]  = this->degCoeff[i]*p3.degCoeff[j];
+            p2.degCoeff[j]  = this->degCoeff[i]*p3.degCoeff[j];
             }
             else
             {
                 for(j=0;j<p3.capacity;j++)
                 {
-                   p1.degCoeff[j] = p3.degCoeff[j];
+                   p2.degCoeff[j+i] = p3.degCoeff[j];
                 }
-                for(j=0;j<p3.capacity;j++)
+                for(j=0;j<p3.capacity+i;j++)
                 {
-                    p1.degCoeff[j] = this->degCoeff[i]*newArray[j];
+                    p2.degCoeff[j] = this->degCoeff[i]*p2.degCoeff[j];
                 }
                 
             }
-            
+            p1 = p1 +p2;
+            p2 = empty;
             i++;
         }
         
-        polynomial p4;
-        for(i=0;i<this->capacity;i++)
-        {
-            p4 = p4 + p1[i];
         
+        return p1;
+    }
+    
+    void setCoefficient(int indexDegree , int coefficient)
+    {
+        if(indexDegree >= capacity)
+        {
+            
+            int *temp;
+            temp = new int[indexDegree+1];
+            int i=0;
+            for(i=0;i<capacity;i++)
+                temp[i] = degCoeff[i];
+            int j=0;
+            for(j=i;j<indexDegree+1;j++)
+                temp[j] = 0;
+            degCoeff = temp;
+            degCoeff[indexDegree] = coefficient;
         }
-        return p4;
+        else
+        {
+           
+            degCoeff[indexDegree] = coefficient;
+        }
+        
+        
+        
+    }
+    void arrayele()
+    {
+        int i=0;
+       
+        for(i=0;i<capacity;i++)
+        cout<<degCoeff[i]<<" ";
+        cout<<endl;
     }
     
     
 };
+
 
